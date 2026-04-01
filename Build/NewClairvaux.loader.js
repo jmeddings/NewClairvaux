ebfd13abf79d784b4f97c2cd5a82fe59ae2a1d56
jpmeddings <jpmeddings@mail.csuchico.edu>
2026-04-01 14:12:52 -0700

    changed to my scene

--- Build/NewClairvaux.loader.js
+++ Build/NewClairvaux.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 55377164,
+    wasmFileSize: 55362833,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
